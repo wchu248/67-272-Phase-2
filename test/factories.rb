c1c022365b325b93d90118590857194f24ec47bb
Winston Chu <wpchu@andrew.cmu.edu>
2017-02-12 18:25:30 -0500

    installed timeliness gem and did more test stuff

---
+++ test/factories.rb
@@ -0,0 +1,26 @@
+FactoryGirl.define do
+  
+  factory :item do
+    name "Wooden Chess Pieces"
+    description "A nice set of chess pieces made out of finished wood."
+    category "pieces"
+    color "tan/beige"
+    weight 4.3
+    inventory_level 50
+    reorder_level 20
+    active true
+  end
+  
+  factory :item_price do
+    price 15.99
+    start_date Date.today
+    end_date nil
+    association :item
+  end
+  
+  factory :purchase do
+    quantity 2
+    date 2.months.ago.to_date
+  end
+
+end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -2,6 +2,12 @@ require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
 
+
+  test "should not save article without title" do
+    item = item.new
+    assert_not item.save
+  end
+
   # Relationship macros...
   should have_many(:item_prices)
   should have_many(:purchases)
@@ -11,7 +17,7 @@ class ItemTest < ActiveSupport::TestCase
   should validate_presence_of(:weight)
   should validate_presence_of(:reorder_level)
   should validate_presence_of(:inventory_level)
-  
-   
+
+
 
 end
